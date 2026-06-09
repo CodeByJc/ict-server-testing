4fbcdd280fcd35b0de314ea53e53b93648296a1f
Codebyjc <jaineelchhatraliya@gmail.com>
2026-06-09 08:52:16 +0530

    env issue solved

--- api/db/db_connection.php
+++ api/db/db_connection.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__,2));
 $dotenv->load();
 
 $hostname = $_ENV['DB_HOST'];
