a9def95ef82f654365c5ae57d8123be8eedb6e85
Ubuntu <ubuntu@ip-172-31-41-93.ap-south-1.compute.internal>
2026-06-13 14:20:07 +0000

    Fix dotenv path loading

--- api/db/db_connection.php
+++ api/db/db_connection.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__,2));
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2), '.env');
 $dotenv->load();
 
 $hostname = $_ENV['DB_HOST'];
